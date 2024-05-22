fd56d023f4b901d4a6039e2fcfc5c8f33e1a97fc
luislrl <luiseduardolrlima@gmail.com>
2024-05-21 23:38:48 -0300

    dia-01

---
+++ dia-1/dia-1.py
@@ -0,0 +1,4 @@
+city = input("Wellcome to the Band Name Generator. \nWhat's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+print("Your band name could be " + city + " " + pet_name)
